eddab3038cb8f905b1508d924f67029771194cd1
Corey Rowe <CoreyR1196@gmail.com>
2016-11-29 14:45:00 -0500

    Add text and forms

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -10,7 +10,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Color Picker</h2>
         </div>
-        <p className="App-intro">
+        <p className="App-converter">
+          <h3>Pick your favorite swatch</h3>
+          
+          <h4>or</h4>
+          <h3>Enter a color code</h3>
+          <form>
+            <select>
+              <option selected value="Hex">Hex</option>
+              <option value="RGB">RGB</option>
+            </select>
+            <label>
+              
+               <input type="text" name="hex" />
+            </label>
+            <input type="Submit" value="Pick!" />
+          </form>
         </p>
       </div>
     );
