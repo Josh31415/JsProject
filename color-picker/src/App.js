c321244e80c50cb1fb563fba5f9cdfd14a503208
Corey Rowe <CoreyR1196@gmail.com>
2016-11-30 12:09:42 -0500

    Add color values to swatches

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -15,11 +15,11 @@ class App extends Component {
           <h3>Pick your favorite swatch</h3>
           <Grid>
             <Row className="show-grid App-swatch-grid">
-              <Col className="App-swatch-col-1" xs={2} md={2}></Col>
-              <Col className="App-swatch-col-2" xs={2} md={2}></Col>
-              <Col className="App-swatch-col-3" xs={2} md={2}></Col>
-              <Col className="App-swatch-col-4" xs={2} md={2}></Col>
-              <Col className="App-swatch-col-5" xs={2} md={2}></Col>
+              <Col className="App-swatch-col-1" xs={2} md={2}>#0000ff</Col>
+              <Col className="App-swatch-col-2" xs={2} md={2}>#008000</Col>
+              <Col className="App-swatch-col-3" xs={2} md={2}>#ffff00</Col>
+              <Col className="App-swatch-col-4" xs={2} md={2}>#ffa500</Col>
+              <Col className="App-swatch-col-5" xs={2} md={2}>#ff0000</Col>
             </Row>
           </Grid>
         </div>
@@ -31,7 +31,7 @@ class App extends Component {
             <option value="RGB">RGB</option>
           </select>
           <label>
-            
+
               <input type="text" name="hex" />
           </label>
           <input type="Submit" value="Pick!" />
