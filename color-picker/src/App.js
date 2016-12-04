95026bd1643137966c36d6fdf71c34679d830744
Corey Rowe <CoreyR1196@gmail.com>
2016-12-03 20:19:52 -0500

    Switch to react-mdl
    Remove react-bootstrap dependency and use hosted stylesheet

--- color-picker/src/App.js
+++ color-picker/src/App.js
@@ -1,42 +1,44 @@
 import React, { Component } from 'react';
-import {Grid, Row, Col} from 'react-bootstrap';
+import {Button, Cell, Grid, Radio, RadioGroup, Textfield} from 'react-mdl';
 import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Color Picker</h2>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to Color Picker</h2>
+          </div>
+          <div className="App-converter">
+            <h3>Pick your favorite swatch</h3>
+            <Grid>
+              <Cell className="App-swatch-col-1" col={1.5} shadow={2} >#0000ff</Cell>
+              <Cell className="App-swatch-col-2" col={1.5} shadow={2}>#008000</Cell>
+              <Cell className="App-swatch-col-3" col={1.5} shadow={2}>#ffff00</Cell>
+              <Cell className="App-swatch-col-4" col={1.5} shadow={2}>#ffa500</Cell>
+              <Cell className="App-swatch-col-5" col={1.5} shadow={2}>#ff0000</Cell>
+            </Grid>
+          </div>
+          <p>
+            <Button raised accent ripple>Pick</Button>
+          </p>
+          <div>
+            <h4>or</h4>
+            <h3>Enter a color code</h3>
+          </div>
+          <div>
+            <RadioGroup name="demo" value="opt1" >
+              <Radio value="opt1" className="App-left-right-padding">Hex</Radio>
+              <Radio value="opt2">RGB</Radio>
+            </RadioGroup>
+            <Textfield name="hex" label="Enter color value" floatingLabel  className="App-padded"/>
+          </div>
+          <p>
+            <Button raised accent ripple>Pick</Button>
+          </p>
         </div>
-        <div className="App-converter">
-          <h3>Pick your favorite swatch</h3>
-          <Grid>
-            <Row className="show-grid App-swatch-grid">
-              <Col className="App-swatch-col-1" xs={2} md={2}>#0000ff</Col>
-              <Col className="App-swatch-col-2" xs={2} md={2}>#008000</Col>
-              <Col className="App-swatch-col-3" xs={2} md={2}>#ffff00</Col>
-              <Col className="App-swatch-col-4" xs={2} md={2}>#ffa500</Col>
-              <Col className="App-swatch-col-5" xs={2} md={2}>#ff0000</Col>
-            </Row>
-          </Grid>
-        </div>
-        <h4>or</h4>
-        <h3>Enter a color code</h3>
-        <form>
-          <select>
-            <option selected value="Hex">Hex</option>
-            <option value="RGB">RGB</option>
-          </select>
-          <label>
-
-              <input type="text" name="hex" />
-          </label>
-          <input type="Submit" value="Pick!" />
-        </form>
-      </div>
     );
   }
 }
